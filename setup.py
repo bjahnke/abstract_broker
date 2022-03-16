6425c288632bed48163af3d177542fe2fd663862
bjahnke <bjahnke71@gmail.com>
2022-03-16 12:29:16 -0400

    added project structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='abstract_broker',
+    version='0.0.1',
+    packages=['abstract'],
+    url='',
+    license='',
+    author='bjahnke',
+    author_email='bjahnke71@gmail.com',
+    description='Interfaces for normalizing broker client/stream APIs',
+    install_requires=[
+        "numpy",
+        "pandas",
+    ]
+)
